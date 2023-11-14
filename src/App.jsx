e29e29e5ec3b0c8ac15906f11632384a1843b8fe
Vladyslav Bozhuk <94122235+escape23@users.noreply.github.com>
2023-11-14 16:49:39 +0100

    UPD

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import Component from "react";
 import Counter from "./components/Counter/Counter";
+import Modal from "./components/Modal/Modal";
 
-const App = () => {
-  return (
-    <div>
-      <Counter />
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    isShowModal: false,
+  };
+  render() {
+    return (
+      <div>
+        <Counter />
+        <Modal></Modal>
+      </div>
+    );
+  }
+}
 
 export default App;
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Counter />
+//       <Modal></Modal>
+//     </div>
+//   );
+// };
+
+// export default App;
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,32 +1,67 @@
-const Counter = () => {
-  let counter = 0;
+import { Component } from "react";
 
-  const handleClick = (e) => {
-    console.log("e", e);
+class Counter extends Component {
+  state = {
+    counter: 0,
   };
-  return (
-    <div className="position-absolute top-50 start-50 translate-middle">
-      <div
-        className="card bg-dark text-white rounded-4"
-        style={{ width: "600px" }}>
-        <div className="card-body">
-          <h5 className="card-title text-center fs-1">Counter</h5>
-          <p className="card-text  text-center" style={{ fontSize: "80px" }}>
-            {counter}
-          </p>
-          <div className="d-flex justify-content-center px-5">
-            <button
-              className="btn btn-outline-success me-5 px-4 rounded-4"
-              onClick={handleClick}>
-              <i className="bi bi-plus-circle fs-1"></i>
-            </button>
-            <button className="btn  btn-outline-danger ms-5 px-4 rounded-4">
-              <i className="bi bi-dash-circle fs-1"></i>
-            </button>
+
+  //   handleIncrement = () => {
+  //     // this.state.counter++;
+  //     // console.log(this.state.counter);
+  //     // this.setState({
+  //     //   counter: 1,
+  //     // });
+  //     this.setState((prevState) => {
+  //       return {
+  //         counter: prevState.counter + 1,
+  //       };
+  //     });
+  //   };
+
+  //   handleDecrement = () => {
+  //     this.setState((prevState) => {
+  //       return {
+  //         counter: prevState.counter - 1,
+  //       };
+  //     });
+  //   };
+
+  handleClick = (isIncrement) => {
+    this.setState((prevState) => {
+      return {
+        counter: isIncrement ? prevState.counter + 1 : prevState.counter - 1,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="position-absolute top-50 start-50 translate-middle">
+        <div
+          className="card bg-dark text-white rounded-4"
+          style={{ width: "600px" }}>
+          <div className="card-body">
+            <h5 className="card-title text-center fs-1">Counter</h5>
+            <p className="card-text  text-center" style={{ fontSize: "80px" }}>
+              {this.state.counter}
+            </p>
+            <div className="d-flex justify-content-center px-5">
+              <button
+                className="btn btn-outline-success me-5 px-4 rounded-4"
+                onClick={() => this.handleClick(true)}>
+                <i className="bi bi-plus-circle fs-1"></i>
+              </button>
+              <button
+                className="btn  btn-outline-danger ms-5 px-4 rounded-4"
+                onClick={() => this.handleClick()}>
+                <i className="bi bi-dash-circle fs-1"></i>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
 export default Counter;
