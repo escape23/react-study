52d886a75507effd8bfe158382f7210246e85497
Vladyslav Bozhuk <94122235+escape23@users.noreply.github.com>
2023-11-15 17:16:09 +0100

    +++

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,51 @@
-import GetArrElem from './components/GetArrName'
-import Wrapper from './components/Wrapper'
+import { Component } from "react";
+import Counter from "./components/Counter/Counter";
+import Modal from "./components/Modal/Modal";
 
-const App = () => {
-	// return GetArrElem({ age: 20, name: 'Alex' })
-	return (
-		<div>
-			<p>qwrety</p>
-			<h2>qwery</h2>
-			<GetArrElem age='20' name='Alex' />
-			<Wrapper>QQQQQ</Wrapper>
-		</div>
-	)
+class App extends Component {
+  state = {
+    isShowModal: false,
+  };
+
+  handleOpen = () => {
+    this.setState({
+      isShowModal: true,
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      isShowModal: false,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <button
+          className="btn btn-success mx-auto my-5"
+          onClick={this.handleOpen}>
+          Open Modal
+        </button>
+        <div>
+          <Counter />
+          {this.state.isShowModal && (
+            <Modal close={this.handleClose}>Some body</Modal>
+          )}
+        </div>
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
+
+// const App = () => {
+// 	return (
+// 		<div>
+// 			<Counter />
+// 			<Modal>Some body</Modal>
+// 		</div>
+// 	)
+// }
+// export default App
